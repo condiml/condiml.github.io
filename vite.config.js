e2c3be6d070a205c248b758e2590cd55a6874671
peterbenoit <peterbenoit@gmail.com>
2025-04-30 09:18:53 -0400

    Add Safari pinned tab icon and web app manifest
    - Created a new SVG file for the Safari pinned tab icon.
    - Added a web app manifest file with metadata including app name, description, icons, theme color, and display settings.

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,20 @@ import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-    plugins: [
-        createHtmlPlugin({
-            minify: {
-                collapseWhitespace: true,
-                removeComments: true,
-                removeRedundantAttributes: true,
-                removeScriptTypeAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                useShortDoctype: true,
-            },
-        }),
-    ],
-    build: {
-        outDir: 'dist',
-    },
+	plugins: [
+		createHtmlPlugin({
+			minify: {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				useShortDoctype: true,
+			},
+		}),
+	],
+	build: {
+		outDir: 'dist',
+	},
+	publicDir: 'public',
 });
